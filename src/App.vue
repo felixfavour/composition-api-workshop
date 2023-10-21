d50e36bc47f25d17373231b7d8179bf98f515e4f
felixfavour <felixfavour0@gmail.com>
2023-10-21 10:29:01 +0100

    init: starter code

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ShoppingList />
+  </div>
+</template>
+
+<script>
+import ShoppingList from './components/ShoppingList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingList
+}
+}
+</script>
+
+<style>
+</style>
